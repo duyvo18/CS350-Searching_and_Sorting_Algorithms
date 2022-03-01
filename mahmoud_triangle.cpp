b6a356aefb5f9f0ae3fb916e511b71f3ef2157c6
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-03-01 17:22:31 +0700

    improve documentation in mahmoud, towers

--- mahmoud_triangle.cpp
+++ mahmoud_triangle.cpp
@@ -1,40 +1,59 @@
 #include <iostream>
 #include <algorithm>
 
-int main() {
+int main()
+{
 	/*
-	 * Read the input
+	 * Read input size
 	 */
-	int n;
-	std::cin >> n;
-	
-	int* a = new int[n];
-	for (int i = 0; i < n; ++i) {
-		std::cin >> a[i];
+	int input_size;
+	std::cin >> input_size;
+
+	/*
+	 * Handle edge case
+	 */
+	if (input_size < 3)
+	{
+		std::cout << "NO";
+		return 0;
 	}
 
 	/*
-	 * Sorting (nlogn)
+	 * Read input to array of segment lengths
 	 */
-	std::sort(a, a + n);
+	int *segment_lengths = new int[input_size];
+	for (int i = 0; i < input_size; ++i)
+	{
+		std::cin >> segment_lengths[i];
+	}
+
+	/*
+	 * Sort length array O(nlogn)
+	 */
+	std::sort(segment_lengths, segment_lengths + input_size);
 
 	/*
 	 * Given a < b < c in sorted array
 	 *  -> b+c>a  & a+c>b due to ordering
-	 *  therefore if a+b>c
-	 *   -> a,b,c is non degenerate triangle
+	 *   therefore if a+b>c
+	 *    -> a,b,c is non degenerate triangle
 	 */
-	for (int i = 0; i < n - 2; ++i) {
-		if (a[i] + a[i+1] > a[i+2]) {
+	for (int i = 0; i < input_size - 2; ++i)
+	{
+		if (segment_lengths[i] + segment_lengths[i + 1] > segment_lengths[i + 2])
+		{
 			std::cout << "YES";
-			
-			delete[] a;
+
+			delete[] segment_lengths;
 			return 0;
 		}
 	}
-	
+
+	/*
+	 * Fall-through case
+	 */
 	std::cout << "NO";
 
-	delete[] a;
+	delete[] segment_lengths;
 	return 0;
 }
--- towers.py
+++ towers.py
@@ -1,14 +1,31 @@
-n = int(input())
+'''
+    Ignore size input
+'''
+int(input())
 
-# dict = {input_value: value_frequency}
-dict = {}
-elemInputs = input().split()
-for input in elemInputs:
-	elem = int(input)
-	dict[elem] = dict.get(elem, 0) + 1
 
-total_bars = len(dict) # O(1)
-highest_bar_height = max(dict.values()) # O(n)
+'''
+    bar_heights_freq = {input_value: value_frequency}
+'''
+bar_heights_freq = {}
 
-print(f'{highest_bar_height} {total_bars}')
+elem_inputs = input().split()
+for input in elem_inputs:
+    elem = int(input)
+    bar_heights_freq[elem] = bar_heights_freq.get(elem, 0) + 1
 
+
+'''
+    Number of bars = Number of elements in non-duplicated dictionary
+    O(1)
+'''
+total_bars = len(bar_heights_freq)
+
+'''
+    Highest bar height = Largest frequency value
+    O(n)
+'''
+highest_bar_height = max(bar_heights_freq.values())
+
+
+print(f"{highest_bar_height} {total_bars}")
