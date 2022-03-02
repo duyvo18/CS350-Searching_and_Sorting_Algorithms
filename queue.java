4bb64458a68ec92a9cdb01f7860b1c575e4146b6
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-03-02 16:42:57 +0700

    queue O(nlogn) in java idea

--- queue.java
+++ queue.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class queue {
     public static void main(String[] args) {
         try (Scanner scanner_in = new Scanner(System.in)) {
             /*
-             * Read input size
+             * Read size input
              */
             int input_size = Integer.parseInt(scanner_in.nextLine());
 
             /*
              * Read customer waiting times
              */
-            int customer_waiting_times[] = new int[input_size];
+            ArrayList<Integer> customer_waiting_times = new ArrayList<>();
             String elem_inputs[] = scanner_in.nextLine().split(" ");
-            for (int i = 0; i < input_size; ++i) {
-                customer_waiting_times[i] = Integer.parseInt(elem_inputs[i]);
+            for (String elem : elem_inputs) {
+                customer_waiting_times.add(Integer.parseInt(elem));
             }
 
             /*
              * Sort customer waiting times
              * O(nlogn)
              */
-            Arrays.sort(customer_waiting_times);
-            
+            customer_waiting_times.sort(null);
+
             /*
              * The customer is satisfied if
              * the elapsed time before him (sum of waiting times prior)
@@ -31,14 +31,32 @@ public class queue {
              */
             int elapsed = 0;
             int satisfy_customers = 0;
-            for (int time : customer_waiting_times) {
-                if (elapsed <= time) {
-                    ++satisfy_customers;
-                }
-                elapsed += time;
-            }
+            // use iterator instead of while loop + index access
+            
+            // NOT OPTIMIZED: while loop + ArrayList access is O(n^2)
+            // int i = 0;
+            // while (i < input_size) {
+            //     // Remove customer if cannot serve in time
+            //     if (elapsed > customer_waiting_times.get(i)) {
+            //         customer_waiting_times.remove(i);
+            //         --input_size;
+            //         continue;
+            //     } else {
+            //         ++satisfy_customers;
+            //         elapsed += customer_waiting_times.get(i);
+            //         ++i;
+            //     }
+            // }
 
-            System.out.println(satisfy_customers);
+            // NOT OPTIMIZED: unsatisfy customers increase unnecessary waiting time
+            // for (int time : customer_waiting_times) {
+            // if (elapsed <= time) {
+            // ++satisfy_customers;
+            // }
+            // elapsed += time;
+            // }
+
+            System.out.print(satisfy_customers);
         }
     }
 }
