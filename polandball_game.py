4d40930f96fd7cfb0caa5484878b637bf10a1abc
vkduy19 <64194955+vkduy19@users.noreply.github.com>
2022-03-08 17:23:24 +0700

    PolandBall game O(n)_O(n) python solution

--- polandball_game.py
+++ polandball_game.py
@@ -1,20 +1,55 @@
 def main():
     polandball_word_num, enemyball_word_num = map(int, input().split(maxsplit=1))
-    
+
+    polandball_words = [input() for _ in range(polandball_word_num)]
+    enemyball_words = [input() for _ in range(enemyball_word_num)]
+
     if enemyball_word_num > polandball_word_num:
-        print('NO')
+        print("NO")
         return
     elif polandball_word_num > enemyball_word_num:
-        print('YES')
+        print("YES")
         return
     else:
-        polandball_words = [input() for _ in range(polandball_word_num)]
-        enemyball_words = [input() for _ in range(enemyball_word_num)]
-        
-        # poland_num_of_repeated_words >= enemy_num_of_repeated_words -> poland wins
-        
-        print('YES')
+        '''
+            Find the freqency of words that both balls know
+            
+            Time: O(n)
+            Space: O(n)
+        '''
+        repeated_words = {}
+
+        for word in polandball_words:
+            repeated_words[word] = repeated_words.get(word, 0) + 1
+
+        for word in enemyball_words:
+            repeated_words[word] = repeated_words.get(word, 0) + 1
+
+
+        '''
+            Find the number of words that both balls know
+            
+            Time: O(n)
+            Space: O(1)
+        '''
+        num_of_repeated_words = 0
+        for _, freq in repeated_words.items():
+            if freq > 1:
+                num_of_repeated_words += 1
+
+        '''
+            Given both balls know the same number of words,
+            if the number of words that both balls know is
+            even then PolandBall loses its advantage,
+            otherwise, PolandBall wins
+        '''
+        if num_of_repeated_words % 2 == 0:
+            print("NO")
+        else:
+            print("YES")
+
         return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
